b1cfc90200099555ba3c2b242af7d3acbcd7d9e0
tlrpv12 <park010804@gmail.com>
2024-01-26 16:04:09 +0900

    Feat: '저자 수정 기능 구현'

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ var http = require('http');
 //var fs = require('fs');
 // var qs= require('querystring');
 // var path=require('path');
-// var template = require('./lib/template.js');
+//var template = require('./lib/template.js');
+var url = require('url');
 var topic=require('./lib/topic.js');
+var author=require('./lib/author');
 
 var app = http.createServer(function(request,response){
   var _url = request.url;
@@ -23,9 +25,17 @@ var app = http.createServer(function(request,response){
   }else if(pathname === '/update'){
     topic.update(request,response);  
   }else if(pathname==='/update_process'){
-      topic.update_process
+    topic.update_process(request,response);
   }else if(pathname==='/delete_process'){
     topic.delete_process(request,response);
+  }else if(pathname === '/author'){
+    author.home(request, response);
+  }else if(pathname === '/author/create_process'){
+    author.create_process(request,response);
+  }else if(pathname === '/author/update'){
+    author.update(request, response);
+  }else if(pathname === '/author/update_process'){
+    author.update_process(request,response);
   }
   else{
     response.writeHead(404);
